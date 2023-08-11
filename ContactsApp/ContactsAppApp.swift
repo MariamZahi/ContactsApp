b37a817d5126b554ff6c44e3f11d40aa75c0f730
Mariam Zahid <mzahidmalhi@hotmail.com>
2023-08-11 04:06:48 -0400

    Initial Commit

---
+++ ContactsApp/ContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppApp.swift
+//  ContactsApp
+//
+//  Created by Waqar on 2023-08-11.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
